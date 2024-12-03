8fbd0ecd85ec2045ce97bac85f7cb835f6163901
kevarm <kevinarmas.77@gmail.com>
2024-12-03 12:30:53 +0000

    03/12

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+    session_start();
+    if(isset($_SESSION['user'])){
+        header("Location: /projects/2-DAW/proyecto_restaurantes/categorias.php");
+        exit();
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/projects/2-DAW/proyecto_restaurantes/css/index.css">
+    <title>Login</title>
+</head>
+<body>
+    <form>
+        <label for="user">Usuario: </label>
+        <input type="text" id="user" name="user">
+        <label for="password">Contraseña: </label>
+        <input type="password" id="password" name="password">
+        <input type="submit" value="Acceder">
+    </form>
+</body>
+</html>
